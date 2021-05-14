69b1f79124ac86ab6671cb09f42df2da8bb11a38
muddi00seven <just00seven@gmail.com>
2021-05-14 10:59:35 +0500

    ui update

--- src/components/cards/item.js
+++ src/components/cards/item.js
@@ -5,14 +5,17 @@ const CardItem = ({ data }) => {
         <div>
             <div className="column">
                 <div className="card">
-                    <h3>Current participants {data.participants}</h3>
-                    <h3>Max participants {data.maxparticipants}</h3>
-                    <h3>Contribution amount {data.amount}</h3>
-                    <div>
-                        <button>
+                    <div className="lottercard-top-container">
+                    <h3 className="lottercard-current-part">Current participants: {data.participants}</h3>
+                    <h3 className="lottercard-max-part">Max participants: {data.maxparticipants}</h3>
+                    </div>
+                 
+                    <h3 className="lottercard-">Contribution amount {data.amount}</h3>
+                    <div className="lottercard-">
+                        <button className="lottercard-">
                             Draw Winner
                     </button>
-                        <button>
+                        <button className="lottercard-">
                             Enter Lotto
                     </button>
                     </div>
