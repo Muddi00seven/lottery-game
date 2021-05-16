12f146aaa45facfbcb04513debc0742df6efbecc
MuhammadHammadAhmed <hammad.tco@gmail.com>
2021-05-16 07:48:02 +0500

    removed useeffect from cards component

--- src/components/cards/index.js
+++ src/components/cards/index.js
@@ -7,15 +7,8 @@ import { useStore } from '../../context/GlobalState';
 
 const LotteryCards = () => {
     const [{web3,accounts,lottocontract,lottoPools},dispatch] = useStore();
-    useEffect(()=>{
-        (async ()=>{
-            if(web3 && lottoPools){
-                console.log("Pool in Cards",lottoPools );
-    
-            }
-        })();
-    },[web3,lottoPools,lottocontract])
     console.log("Pool in Cards",lottoPools );
+  
     let lotteryData = dummyData;
     return (
         <div className="lotter-cards-container">
